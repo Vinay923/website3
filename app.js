af153a358045b37c13764a4ee9cd269b0019414b
Vinay923 <abhinay923@gmail.com>
2024-12-06 22:26:08 +0530

    trying fix for server

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'build')));
 app.listen(PORT);
-app.use(cors());
 
 
 
